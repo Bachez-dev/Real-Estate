74eab7917c1139c16761cf1f7e3e290b28ab9c46
Bachez-Dev <bachezgitau@gmail.com>
2025-09-27 03:17:26 -0700

    Retry Real Estate push

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const currentPage = location.pathname.split("/").pop();
+
+const navLinks = document.querySelectorAll(".nav-links a");
+
+navLinks.forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+  }
+});
